f051f38b07de0202fae7134249199f232bc9242e
emge1 <emygeq@gmail.com>
2025-03-12 11:32:47 +0100

    #2 - Feature: Azure in provider.tf for UI

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,9 +4,9 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 5.0"
     }
-    vercel = {
-      source = "vercel/vercel"
-      version = "1.9.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
     }
   }
   required_version = ">= 1.5.0"
@@ -16,6 +16,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "vercel" {
-  api_token = var.vercel_api_token
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {}
 }
